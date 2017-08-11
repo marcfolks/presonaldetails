ce57d4a6ff6e67437b93b38b2709b4237c394a9e
marcfolks <marcfolks@gmail.com>
2017-08-11 11:07:05 -0400

    addedgemfileprocfile

--- Gemfile.rb
+++
@@ -1,5 +0,0 @@
-source "https://rubygems.org"
-#insert your version of ruby(you can check in the command line with ruby -v
-ruby '2.2.6'
-#insert your version of sinatra(you can find it in the command line with gem list
-gem 'sinatra', '~>2.0.0'
